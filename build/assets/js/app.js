b6c309ce2f5d5052e3e1af632558c0b92c770943
Tha Gidi <you@example.com>
2019-09-28 23:43:55 +0100

    made page mobile responsive

--- build/assets/js/app.js
+++ build/assets/js/app.js
@@ -1,1 +1,51 @@
 window.sr=ScrollReveal();
+
+function showNav(){
+    var x = document.getElementById("responsive-nav");
+    if(x.className === "responsive-nav"){
+        x.className += " unfold";
+    } else {
+        x.className = "responsive-nav";
+    }
+}
+
+sr.reveal("#header img.hero-phone", {
+    duration: 500,
+    distance: '100px',
+    origin: 'bottom'
+});
+
+sr.reveal("#features div.car", {
+    duration: 500,
+    delay: 200,
+    distance: '100px',
+    origin: 'bottom'
+});
+
+sr.reveal("#features div.shoe", {
+    duration: 500,
+    delay: 400,
+    distance: '100px',
+    origin: 'bottom'
+});
+
+sr.reveal("#features div.device", {
+    duration: 500,
+    delay: 600,
+    distance: '100px',
+    origin: 'bottom'
+});
+
+sr.reveal("#discount .offer", {
+    duration: 500,
+    delay: 600,
+    distance: '200px',
+    origin: 'right'
+});
+
+sr.reveal("#discount .discount-phone", {
+    duration: 500,
+    delay: 600,
+    distance: '200px',
+    origin: 'left'
+});
